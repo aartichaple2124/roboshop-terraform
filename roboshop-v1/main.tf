6f79674d2aa760b750052e4ab293c5e08f468ab3
LP269 <aartichaple2124@gmail.com>
2025-05-09 20:39:13 +0530

    Mongodb push

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,9 +1,57 @@
-module "frontend" {
+module "components" {
+  for_each = var.components
   source = "./vm"
-  component = "frontend"
+  component = each.value["nname"]
+  vm_size = each.value["nname"]
 }
 
-module "catalogue" {
-  source = "./vm"
-  component = "catalogue"
+variable "components" {
+  default = {
+
+    frontend = {
+      name = frontend
+      vm_size = "Standard_D2as_v4"
+    }
+
+    mongodb = {
+      name = mongodb
+      vm_size = "Standard_D2as_v4"
+    }
+
+    catalogue = {
+      name = catalogue
+      vm_size = "Standard_D2as_v4"
+    }
+
+    user = {
+      name = user
+      vm_size = "Standard_D2as_v4"
+    }
+
+    cart = {
+      name = cart
+      vm_size = "Standard_D2as_v4"
+    }
+
+    mysql = {
+      name = mysql
+      vm_size = "Standard_D2as_v4"
+    }
+
+    shipping = {
+      name = shipping
+      vm_size = "Standard_D2as_v4"
+    }
+
+    payment = {
+      name = payment
+      vm_size = "Standard_D2as_v4"
+    }
+
+    redis = {
+      name = redis
+      vm_size = "Standard_D2as_v4"
+    }
+  }
+
 }
