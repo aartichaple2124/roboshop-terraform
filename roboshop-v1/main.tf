a405a4db1ce15f7c82447f17ec6ae7685375f0a1
LP269 <aartichaple2124@gmail.com>
2025-05-12 17:14:24 +0530

    Mongodb push

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -2,7 +2,7 @@ module "components" {
   for_each = var.components
   source = "./vm"
   component = each.value["name"]
-  vm_size = each.value["name"]
+  vm_size = each.value["vm_size"]
 }
 
 variable "components" {
